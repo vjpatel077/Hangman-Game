7cad3b548dce6bc55c7394e97c1cf38541433868
Vijay Kumar Patel <39783317+vjpatel077@users.noreply.github.com>
2024-02-08 10:02:22 +0530

    Adding hangman functions header file

---
+++ hangman_functions.h
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+//declaring greet function
+void greet();
+
+void display_misses(int misses,string clue, int streak);
+
+void display_status(string answer);
+
+bool end_game(string answer, string codeword);
